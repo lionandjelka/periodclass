ea940d8ce16785f5b62785981178234679fb2199
lionandjelka <angelkovacevic@gmail.com>
2023-06-09 10:47:57 +0200

    Update initializer.py

--- periodX/baseperiodicity/periodicity/initializer.py
+++ periodX/baseperiodicity/periodicity/initializer.py
@@ -2,7 +2,7 @@ from .data_loader import DataLoader
 
 def initialize_data_loader(path_source, path_obj):
     global shared_data_loader
-    shared_data_loader = DataLoader()
-    shared_data_loader.load_fs_gp()
+    shared_data_loader = DataLoader()   
     shared_data_loader.load_fs_df(path_source)
+    shared_data_loader.load_fs_gp()
     shared_data_loader.load_object_df(path_obj)
