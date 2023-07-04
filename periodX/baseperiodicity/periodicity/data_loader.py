c9adcc59f4394bdbc71f4e55819e4fdb49244ead
lionandjelka <angelkovacevic@gmail.com>
2023-07-04 16:42:35 +0200

    Update data_loader.py

--- periodX/baseperiodicity/periodicity/data_loader.py
+++ periodX/baseperiodicity/periodicity/data_loader.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from multiprocessing import Manager
+import pandas as pd
 
 class DataLoader:
     def __init__(self, path_source, path_obj, shared_data):
@@ -12,11 +13,10 @@ class DataLoader:
         self.shared_data['fs_df_path'] = self.path_source
     
     def load_fs_gp(self):
-        # Load fs_df from the file path and create fs_gp
+        # Load fs_df from the file path and store the necessary information for fs_gp
         fs_df_path = self.shared_data['fs_df_path']
         fs_df = pd.read_parquet(fs_df_path)
-        fs_gp = fs_df.groupby('objectId')
-        self.shared_data['fs_gp'] = fs_gp
+        self.shared_data['fs_df_groupby_column'] = 'objectId'
     
     def load_object_df(self):
         # Set the file path for object_df and td_objects in the shared_data
@@ -24,16 +24,16 @@ class DataLoader:
     
     def get_loaded_data(self):
         # Load the actual data (fs_df, object_df, td_objects, fs_gp) when needed
-        fs_df_path = self.shared_data.get('fs_df_path')
+        fs_df_path = self.shared_data['fs_df_path']
         fs_df = pd.read_parquet(fs_df_path)
         
-        object_df_path = self.shared_data.get('object_df_path')
+        object_df_path = self.shared_data['object_df_path']
         object_df = pd.read_parquet(object_df_path)
         
         lc_cols = [col for col in object_df.columns if 'Periodic' in col]
         td_objects = object_df.dropna(subset=lc_cols, how='all').copy()
         
-        fs_gp = self.shared_data.get('fs_gp')
+        fs_gp = fs_df.groupby(self.shared_data['fs_df_groupby_column'])
         
         return fs_df, object_df, td_objects, fs_gp
 
