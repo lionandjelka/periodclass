d1b353a990eede03c582f7053f63d7989de1e632
lionandjelka <angelkovacevic@gmail.com>
2023-07-04 11:49:57 +0200

    Delete initializer.py

--- periodX/baseperiodicity/periodicity/initializer.py
+++
@@ -1,8 +0,0 @@
-from .data_loader import DataLoader
-
-def initialize_data_loader(path_source, path_obj):
-    global shared_data_loader
-    shared_data_loader = DataLoader()   
-    shared_data_loader.load_fs_df(path_source)
-    shared_data_loader.load_fs_gp()
-    shared_data_loader.load_object_df(path_obj)
