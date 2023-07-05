7a7829e38f63aee26ef6da0084be4bffe1b163ed
lionandjelka <angelkovacevic@gmail.com>
2023-07-05 16:23:58 +0200

    Update data_loader.py

--- periodX/baseperiodicity/periodicity/data_loader.py
+++ periodX/baseperiodicity/periodicity/data_loader.py
@@ -6,6 +6,9 @@ class DataLoader:
         self.path_source = path_source
         self.path_obj = path_obj
         self.shared_data = shared_data
+        self.fs_gp = None
+        self.fs_df = None
+        self.object_df = None
     
     def load_fs_df(self):
         # Set the file path for fs_df in the shared_data
@@ -14,8 +17,9 @@ class DataLoader:
     def load_fs_gp(self):
         # Load fs_df from the file path and store the necessary information for fs_gp
         fs_df_path = self.shared_data['fs_df_path']
-        fs_df = pd.read_parquet(fs_df_path)
+        self.fs_df = pd.read_parquet(fs_df_path)
         self.shared_data['fs_df_groupby_column'] = 'objectId'
+        self.fs_gp = self.fs_df.groupby(self.shared_data['fs_df_groupby_column'])
     
     def load_object_df(self):
         # Set the file path for object_df and td_objects in the shared_data
@@ -24,31 +28,15 @@ class DataLoader:
     def get_loaded_data(self):
         # Load the actual data (fs_df, object_df, td_objects, fs_gp) when needed
         fs_df_path = self.shared_data['fs_df_path']
-        fs_df = pd.read_parquet(fs_df_path)
+        self.fs_df = pd.read_parquet(fs_df_path)
         
         object_df_path = self.shared_data['object_df_path']
-        object_df = pd.read_parquet(object_df_path)
+        self.object_df = pd.read_parquet(object_df_path)
         
-        lc_cols = [col for col in object_df.columns if 'Periodic' in col]
-        td_objects = object_df.dropna(subset=lc_cols, how='all').copy()
+        lc_cols = [col for col in self.object_df.columns if 'Periodic' in col]
+        td_objects = self.object_df.dropna(subset=lc_cols, how='all').copy()
         
-        fs_gp = fs_df.groupby(self.shared_data['fs_df_groupby_column'])
+        self.fs_gp = self.fs_df.groupby(self.shared_data['fs_df_groupby_column'])
         
-        return fs_df, object_df, td_objects, fs_gp
-    
-
-
-#if __name__ == '__main__':
-#    manager = Manager()
-#    shared_data = manager.dict()
-    
-#    loader = DataLoader('path/to/source', 'path/to/objects', shared_data)
-#    loader.load_fs_df()
-#    loader.load_fs_gp()
-#    loader.load_object_df()
-    
-    # Access the loaded DataFrames and fs_gp using the get_loaded_data method
-#    fs_df, object_df, td_objects, fs_gp = loader.get_loaded_data()
-    
-    # Use fs_df, object_df, td_objects, fs_gp in other classes/functions as needed
+        return self.fs_df, self.object_df, td_objects, self.fs_gp
 
