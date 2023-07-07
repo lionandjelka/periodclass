32256143121a0f4a3b54409c0a0f9d6cb0a37df5
lionandjelka <angelkovacevic@gmail.com>
2023-07-07 18:48:22 +0200

    Update loader_helper.py

--- periodX/baseperiodicity/periodicity/loader_helper.py
+++ periodX/baseperiodicity/periodicity/loader_helper.py
@@ -1,21 +1,16 @@
 from multiprocessing import Manager
 from periodicity.data_loader import DataLoader
+from periodicity import globalss
 
 def load_and_get_data(path_source, path_obj):
-    # Create a shared dictionary using Manager
     manager = Manager()
     shared_data = manager.dict()
-
-    # Initialize the DataLoader
-    loader = DataLoader(path_source, path_obj, shared_data)
-
-    # Load the data
+    
+    loader = DataLoader('path/to/source', 'path/to/objects', shared_data)
     loader.load_fs_df()
     loader.load_fs_gp()
     loader.load_object_df()
-
+    
     # Access the loaded DataFrames and fs_gp using the get_loaded_data method
     fs_df, object_df, td_objects, fs_gp = loader.get_loaded_data()
-
-    # Return the loaded data
     return fs_df, object_df, td_objects, fs_gp
