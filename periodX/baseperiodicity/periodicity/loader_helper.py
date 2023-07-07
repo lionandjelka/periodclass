39d25fd05d54a6c055d5cc0e42ee532151cf31a3
lionandjelka <angelkovacevic@gmail.com>
2023-07-07 18:50:54 +0200

    Update loader_helper.py

--- periodX/baseperiodicity/periodicity/loader_helper.py
+++ periodX/baseperiodicity/periodicity/loader_helper.py
@@ -6,11 +6,12 @@ def load_and_get_data(path_source, path_obj, shared_data):
     manager = Manager()
     shared_data = manager.dict()
     
-    loader = DataLoader(path_source, path_objects, shared_data)
+    loader = DataLoader(path_source, path_obj, shared_data)
     loader.load_fs_df()
     loader.load_fs_gp()
     loader.load_object_df()
     
     # Access the loaded DataFrames and fs_gp using the get_loaded_data method
     fs_df, object_df, td_objects, fs_gp = loader.get_loaded_data()
+    
     return fs_df, object_df, td_objects, fs_gp
