11b0ef3d50ac22bde88117d5026bd80ef23fff37
lionandjelka <angelkovacevic@gmail.com>
2023-07-08 12:26:03 +0200

    Update globalss.py

--- periodX/baseperiodicity/periodicity/globalss.py
+++ periodX/baseperiodicity/periodicity/globalss.py
@@ -1,5 +1,6 @@
-# Define global variables
+import pandas as pd
+#Define global variables
+fs_df = pd.DataFrame()
+object_df = pd.DataFrame()
+td_objects = pd.DataFrame()
 fs_gp = None
-fs_df = None
-object_df = None
-td_objects = None
